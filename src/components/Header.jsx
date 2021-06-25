6146bd834995ecba75c4821241e4f5910901ceaf
iainbrux <me@iainbrux.com>
2021-06-25 21:49:58 +0100

    finish navbar and styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,17 +3,30 @@ import '../styles/Header.css'
 
 const Header = ({ setDisplay }) => {
 
+  const removeClassName = () => {
+    const active = document.querySelector('.active');
+    active.classList.remove('active');
+  }
+
+  const scrollToTop = () => {
+    document.body.scrollTop = 0; //for Safari
+    document.documentElement.scrollTop = 0; //for all others
+  }
+
   const handleClick = (e) => {
     e.preventDefault();
+    removeClassName();
+    scrollToTop();
+    e.target.classList.add('active')
     setDisplay(e.target.innerText.toLowerCase());
   }
 
   return (
     <header>
-      <h1><span className="pink">IAB</span>.dev</h1>
+      <h1><span className="pink">iain</span>.biz</h1>
       <nav>
         <ul>
-          <li><a href="/" onClick={handleClick}>HOME</a></li>
+          <li><a href="/" onClick={handleClick} className="active">HOME</a></li>
           <li><a href="/about" onClick={handleClick}>ABOUT</a></li>
           <li><a href="/contact" onClick={handleClick}>CONTACT</a></li>
         </ul>
