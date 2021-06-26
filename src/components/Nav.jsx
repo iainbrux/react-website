864dcaff7bfb10664d5ade27dacc55b867025506
iainbrux <me@iainbrux.com>
2021-06-27 00:43:46 +0100

    box shadow improvements

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,8 +6,8 @@ const Nav = ({backToHome, handleClick}) => {
       <h1 onClick={backToHome}><span className="green">iain</span><span className="pink">.biz</span></h1>
        <nav>
         <ul>
-          <li><a href="/" onClick={handleClick} className="active">home</a></li>
-          <li><a href="/portfolio" onClick={handleClick}>portfolio</a></li>
+          <li><a href="/" onClick={handleClick}>home</a></li>
+          <li><a href="/portfolio" onClick={handleClick} className="active">portfolio</a></li>
           <li><a href="/about" onClick={handleClick}>about</a></li>
           <li><a href="/contact" onClick={handleClick}>contact</a></li>
         </ul>
