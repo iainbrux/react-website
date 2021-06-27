90e40c13002f2c4dea0b60e265818b24dd9d0f03
iainbrux <me@iainbrux.com>
2021-06-28 00:46:41 +0100

    created random blob logic and layerover desc

--- src/components/composables/Project.jsx
+++ src/components/composables/Project.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import blob from '../../img/blob.svg'
+import blob2 from '../../img/blob2.svg'
 
 const Project = ({ wrapper, description }) => {
+  const blobs = [blob, blob2];
+
+  const randomBlob = () => {
+    const random = Math.floor(Math.random() * 2)
+    return blobs[random]
+  }
+
   return (
     <div className="project">
       <div className={wrapper}>
         <img src="https://via.placeholder.com/800x350" alt="" />
         <div className={description}>
+          <img src={randomBlob()} alt="" className="blob" />
           <h3>My project</h3>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
